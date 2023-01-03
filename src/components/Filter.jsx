7177d7f9dcdb22bd21c7b2de073ec6e87081849e
Maria Porcina <maria.porcina.mp@gmail.com>
2023-01-03 18:02:37 -0300

    initial structure for the filter component

---
+++ src/components/Filter.jsx
@@ -0,0 +1,36 @@
+import { useState, useEffect } from "react";
+
+import products from '../products.json';
+
+function Filter() {
+    const [productsList, setProducts] = useState(products.data.nodes);
+    const [categories, setCategories] = useState([]);
+
+    const getCategories = () => {
+        const allCategories = [];
+
+        productsList.forEach((product)=>{
+            const categoryId = product.category._id;
+            const categoryName = product.category.name; 
+        
+            allCategories[categoryId] = categoryName;
+        });
+
+        setCategories(allCategories);
+    }
+
+    useEffect(getCategories, []);
+
+    const options = [];
+    for(const category in categories) {
+        options.push(<option key={ category } value={ categories[category] }>{ categories[category] }</option>);
+    }
+
+    return (
+        <select name="categories">
+            { options }
+        </select>
+    );
+}
+
+export default Filter;
